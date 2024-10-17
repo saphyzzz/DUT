c16f1821993d629d278e4526b4ec5f4deedaebb0
KarolinaisTired <karolinafijalkowska99@gmail.com>
2024-10-17 17:28:17 +0100

    Added red flashing to enemy face in the main menu

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -9,16 +9,21 @@ public class MainMenuController : MonoBehaviour
     public Sprite face;
     public Sprite empty;
     public Sprite eyes;
+    public Sprite redFace;
     public Image dark;
     private int imageLength;
     private int imageChoice;
-    private int extraFlashBreak;
+    private float extraFlashBreak;
+    private float extraFaceFlash;
+    private bool isFace;
 
     // Start is called before the first frame update
     void Start()
     {
         dark.gameObject.SetActive(false);
         StartCoroutine(ChangeImage());
+        StartCoroutine(FlashScreenExtra());
+        StartCoroutine(RedFaceHandler());
     }
 
     // Update is called once per frame
@@ -33,19 +38,22 @@ public class MainMenuController : MonoBehaviour
         //infinite loop
         while (true)
         {
-            imageLength = UnityEngine.Random.Range(1, 10);
+            imageLength = UnityEngine.Random.Range(1, 8);
             StartCoroutine(FlashScreen());
             imageChoice = UnityEngine.Random.Range(1, 4);
             if (imageChoice == 1)
             {
+                isFace = false;
                 background.GetComponent<Image>().sprite = eyes;
             }
             if (imageChoice == 2)
             {
+                isFace = false;
                 background.GetComponent<Image>().sprite = empty;
             }
             if (imageChoice == 3)
             {
+                isFace = true;
                 background.GetComponent<Image>().sprite = face;
             }
 
@@ -68,7 +76,7 @@ public class MainMenuController : MonoBehaviour
             {
                 dark.gameObject.SetActive(false);
             }
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.2f);
         }
     }
 
@@ -76,9 +84,38 @@ public class MainMenuController : MonoBehaviour
     {
         while (true)
         {
-            extraFlashBreak = UnityEngine.Random.Range(1, 5);
+            extraFlashBreak = UnityEngine.Random.Range(0.1f, 5.0f);
             StartCoroutine(FlashScreen());
             yield return new WaitForSeconds(extraFlashBreak);
+        }
+    }
+
+    private IEnumerator FlashRedFace()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (i == 0)
+            {
+                background.GetComponent<Image>().sprite = redFace;
+            }
+            else
+            {
+                background.GetComponent<Image>().sprite = face;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private IEnumerator RedFaceHandler()
+    {
+        while (true)
+        {
+            extraFaceFlash = UnityEngine.Random.Range(0.1f, 2.0f);
+            if (isFace)
+            {
+                StartCoroutine(FlashRedFace());
+            }
+            yield return new WaitForSeconds(extraFaceFlash);
         }
     }
 }
