506756e480f0860360bacf073e1737c710e74b3f
KarolinaisTired <karolinafijalkowska99@gmail.com>
2025-01-02 21:19:58 +0000

    death screen still not working

--- Assets/Scripts/BarbraScript.cs
+++ Assets/Scripts/BarbraScript.cs
@@ -2,7 +2,10 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.UI;
 
 public class TestTrigger : MonoBehaviour
 {
@@ -12,12 +15,16 @@ public class TestTrigger : MonoBehaviour
     public AudioSource source; 
     public AudioClip defeatClip;
     public AudioSource inGameSource;
+    public GameObject panel;
+    public Sprite mouthClosed;
+    public Sprite mouthOpen;
+    private bool isOpen;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOpen = false;
     }
 
     // Update is called once per frame
@@ -31,8 +38,27 @@ public class TestTrigger : MonoBehaviour
          if(collision.gameObject.name.Contains("Player"))
          { 
             gameManager.currentState = GameManager.GameState.DefeatScreen;
+            StartCoroutine(Laugh());
             source.Play();
-            inGameSource.Stop(); 
+            inGameSource.Stop();
          }
       }
+
+    private IEnumerator Laugh()
+    {
+        while (true)
+        {
+            if (isOpen)
+            {
+                panel.GetComponent<Image>().sprite = mouthClosed;
+                isOpen = false;
+            }
+            else
+            {
+                panel.GetComponent<Image>().sprite = mouthOpen;
+                isOpen = true;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
 }
