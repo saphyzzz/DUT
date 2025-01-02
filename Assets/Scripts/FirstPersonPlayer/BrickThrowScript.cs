6d8c23aaa2f2c2734e34b057b90b74605ee84833
Avalon Brathwaite <virlinthejammer@gmail.com>
2025-01-02 17:36:16 +0000

    start of lighting

--- Assets/Scripts/FirstPersonPlayer/BrickThrowScript.cs
+++ Assets/Scripts/FirstPersonPlayer/BrickThrowScript.cs
@@ -9,12 +9,13 @@ public class BrickThrowScript : MonoBehaviour
     public GameObject brick;
     public GameObject orientation;
     private GameObject newBrick;
+    public GameManager gameManager;  
     public Player player;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && player.brickCount != 0)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && player.brickCount != 0 && gameManager.currentState != GameManager.GameState.PauseMenu)
         {
             newBrick = Instantiate(brick, new Vector3 (orientation.transform.position.x, (orientation.transform.position.y)-0.5f, orientation.transform.position.z), orientation.transform.rotation);
             newBrick.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;
